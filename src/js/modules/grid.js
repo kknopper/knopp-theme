b17c475d0241edf6e8deb400a4f92667258e7136
Kevin Knopp <kknopper@gmail.com>
2017-03-20 22:20:41 -0700

    adjusted footer logo, added bgimage

--- src/js/modules/grid.js
+++ src/js/modules/grid.js
@@ -1,7 +1,8 @@
 import $ from "jquery";
 import { debug } from "./debug";
-
 import mixitup from 'mixitup';
+import imagesLoaded from 'imagesloaded';
+imagesLoaded.makeJQueryPlugin($);
 // jQuery.ready(function() {
 // 	console.log('add filterizr')
 // 	require('filterizr');
@@ -9,7 +10,8 @@ import mixitup from 'mixitup';
 
 function grid() {
 	let $grid = $('.portfolio-grid'),
-	$gridItem = $('.portfolio-piece')
+	$gridItem = $('.portfolio-piece'),
+    $gridExpansions = $('.portfolio-piece-expansion');
 
 	debug('Initiate Grid', 'success');
 
@@ -21,6 +23,17 @@ function grid() {
         	duration: 500
     	}
 	});
+
+    $gridItem.click(function(e) {
+        e.preventDefault();
+        //remove currecnt active states
+        $gridItem.removeClass('active');
+        $(this).addClass('active');
+    })
+
+    $grid.imagesLoaded(function() {
+        debug('Grid images loaded');
+    })
 }
 
 export { grid };
